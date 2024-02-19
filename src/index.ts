ba8ddd5cefb4f1bd293f3e954cca43d1b093b4a1
Gedeon-programmer <twizerimanagedeon273@gmail.com>
2024-02-19 10:19:02 +0200

    clude

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import router from "./routers/index";
+import router from "./router";
 
 
 dotenv.config();
@@ -10,7 +10,7 @@ dotenv.config();
 const andela = express();
 
 andela.use(bodyParser.json());
-// andela.use("/Jant", router);
+andela.use("/Jant", router);
 
 const portas = parseInt(process.env.PORT || "3000", 10);
 const db = process.env.DATABASE || "mongodb+srv://gedeonprogrammer:programmer12@cluster0.8sikbdc.mongodb.net/Andela-poltifolio";
